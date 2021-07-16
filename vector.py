120e98f6153e98d2f676761c579aadb78c617947
K3lk3n <84258161+K3lk3n@users.noreply.github.com>
2021-07-16 15:03:01 +0545

    Edited Docstring

--- vector.py
+++ vector.py
@@ -8,36 +8,36 @@ class Vector(object):
 		self._y = y
 		 	
 	def getX(self):
-		""" returns x value """
+		""" returns x value of vector object"""
 		return self._x
 		
 	def getY(self):
-		""" returns y value """
+		""" returns y value of vector object  """
 		return self._y
 		
 	def setX(self, newvalue):
-		""" sets x value to new value """
+		""" set new value to x  """
 		self._x = newvalue
 		
 	def setY(self, newvalue):
-		""" sets y value to new value """
+		""" sets new value to y  """
 		self._y = newvalue
 		
 	def magnitude(self, other):
-		""" returns magnitude of the vector """
+		""" returns magnitude of the vector object """
 		magnitude_var = math.sqrt((other.getX() - self.getX()) ** 2  + (other.getY() - self.getY()) ** 2)
 		return round(magnitude_var)
 			 			 	
 	def __str__(self):
-		""" calls when used print function """
+		""" returns string version of the vector object """
 		return str(self._x) + ", " + str(self._y)
 		
 	def __repr__(self):
-		""" representation """
+		""" representation of the vector object  """
 		return '({}, {})'.format(str(self._x), str(self._y))
 		
 	def __add__(self, other):
-		""" adds two vector and returns """
+		""" returns the sum of two vectors """
 		return self.getX() + other.getX(), self.getY() + other.getY()
 		
 	def __sub__(self, other):
@@ -45,6 +45,6 @@ class Vector(object):
 		return self.getX() - other.getX(), self.getY() - other.getY()
 		
 	def __mul__(self, other):
-		""" returns the product of the vector multiplied by a scalar """
+		""" returns the product of the vector and a scalar """
 		return self.getX() * other, self.getY() * other
 											
